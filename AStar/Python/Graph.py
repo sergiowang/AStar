13ef29d016294acb3b7e290e85af9527b4ef92e1
unknown <413022472@qq.com>
2019-05-08 14:46:37 +0800

    A star

--- AStar/Python/Graph.py
+++ AStar/Python/Graph.py
@@ -1,26 +1,39 @@
 import geopandas as gpd
 
 class Node(object):
-    def __init__(self, ID):
+    def __init__(self, ID, lon, lat):
         self.ID = ID
-    
+        self.lon = lon
+        self.lat = lat
+        self.parent = None
+
     def set_parent(self, parentNode):
         self.parent = parentNode
 
     def __repr__(self):
-        return 'Node ID:{}, parent:{}'.format(self.ID, self.parent)
+        return 'Node ID:{}, cor:({}, {}), parent:{}'.format(self.ID, self.lon, self.lat, self.parent)
 
 
 class Graph(object):
     def __init__(self, shpFile, encoding):
         self.links = gpd.read_file(shpFile, encoding = encoding)
         self.initNetwork()
-        #print(self.graph)
+        print(self.graph)
 
     def initNetwork(self):
         self.graph = {}
         for link in self.links.values.tolist():
-            linkLength, fromNode, toNode = link[1], Node(link[3]), Node(link[4])
+            linkLength, fromNodeID, toNodeID, geometry = link[1],link[3], link[4], str(link[-1])
+            openBracket = geometry.index('(')
+            closeBracket = geometry.index(')')
+            points = geometry[openBracket+1:closeBracket-1].split(', ')
+            fromNodeCor = points[0].split(' ')
+            fromNodeLon, fromNodeLat = fromNodeCor[0], fromNodeCor[1]
+            toNodeCor = points[1].split(' ')
+            toNodeLon, toNodeLat = toNodeCor[0], toNodeCor[1]
+            fromNode, toNode = Node(fromNodeID, fromNodeLon, fromNodeLat), Node(toNodeID, toNodeLon, toNodeLat)
+            print('from node:{}'.format(fromNode))
+            print('to node:{}'.format(toNode))
             try:
                 self.graph[fromNode].append((toNode, linkLength))
                 print(fromNode)
