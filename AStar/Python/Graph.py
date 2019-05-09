35a8f5fceade6f6fb2fee3dbac62334b0f62947e
unknown <413022472@qq.com>
2019-05-09 10:18:28 +0800

    A star

--- AStar/Python/Graph.py
+++ AStar/Python/Graph.py
@@ -3,42 +3,48 @@ import geopandas as gpd
 class Node(object):
     def __init__(self, ID, lon, lat):
         self.ID = ID
-        self.lon = lon
-        self.lat = lat
+        self.coor = float(lat), float(lon)
         self.parent = None
-
+        self.gScore, self.fScore = float('Inf'), float('Inf')
+        
     def set_parent(self, parentNode):
         self.parent = parentNode
 
+    def show_info(self):
+        return 'Node ID:{}, coor:({}, {}), parent:{}'.format(self.ID, self.lon, self.lat, self.parent)
+
     def __repr__(self):
-        return 'Node ID:{}, cor:({}, {}), parent:{}'.format(self.ID, self.lon, self.lat, self.parent)
+        return 'Node({})'.format(self.ID)
+
 
 
 class Graph(object):
     def __init__(self, shpFile, encoding):
         self.links = gpd.read_file(shpFile, encoding = encoding)
         self.initNetwork()
-        print(self.graph)
+        del self.links
+        print('network init done!')
 
     def initNetwork(self):
-        self.graph = {}
+        """
+        network:{nodeID:{"node":Node, 
+                         "neighbours":[(Node1, dis1), (Node2, dis2), ...]}, 
+                        ...}
+        """
+        self.network = {}
         for link in self.links.values.tolist():
-            linkLength, fromNodeID, toNodeID, geometry = link[1],link[3], link[4], str(link[-1])
-            openBracket = geometry.index('(')
-            closeBracket = geometry.index(')')
+            linkLength, fromNodeID, toNodeID, geometry = link[1],str(link[3]), str(link[4]), str(link[-1])
+            openBracket, closeBracket = geometry.index('('), geometry.index(')')
             points = geometry[openBracket+1:closeBracket-1].split(', ')
-            fromNodeCor = points[0].split(' ')
+            fromNodeCor, toNodeCor = points[0].split(' '), points[1].split(' ')
             fromNodeLon, fromNodeLat = fromNodeCor[0], fromNodeCor[1]
-            toNodeCor = points[1].split(' ')
             toNodeLon, toNodeLat = toNodeCor[0], toNodeCor[1]
             fromNode, toNode = Node(fromNodeID, fromNodeLon, fromNodeLat), Node(toNodeID, toNodeLon, toNodeLat)
-            print('from node:{}'.format(fromNode))
-            print('to node:{}'.format(toNode))
             try:
-                self.graph[fromNode].append((toNode, linkLength))
-                print(fromNode)
+                self.network[fromNode.ID]["neighbours"].append((toNode, linkLength))
             except KeyError:
-                self.graph[fromNode] = [(toNode, linkLength)]
+                self.network[fromNode.ID] = {"node":fromNode, 
+                                             "neighbours":[(toNode, linkLength)]}
         
 
 if __name__ == '__main__':
--- AStar/Python/Python/main.py
+++
@@ -1,5 +0,0 @@
-from Graph import Graph
-from Astar import Astar
-
-g = Graph('E:\java_projects\linkmatch\sz_shp_mars\link', 'gbk')
-a = Astar(g, )
